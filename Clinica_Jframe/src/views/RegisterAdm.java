dd9fbea90b1e3e71f0701d1af4dfc89f68e0c42f
luis243546 <luisangelcalleallcca@gmail.com>
2023-10-25 00:05:25 -0500

    ADD: se hizo registro del Administrative y conexion a la base de datos

--- Clinica_Jframe/src/views/RegisterAdm.java
+++ Clinica_Jframe/src/views/RegisterAdm.java
@@ -1,14 +1,38 @@
 package views;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+import model.DBConnection;
+import javax.swing.JPasswordField;
 
 public class RegisterAdm extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField nameField;
+	private JTextField lastnameField;
+	private JTextField dniField;
+	private JTextField phoneField;
+	private JTextField emailField;
+	private Connection connect;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -19,6 +43,7 @@ public class RegisterAdm extends JFrame {
 				try {
 					RegisterAdm frame = new RegisterAdm();
 					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -30,12 +55,119 @@ public class RegisterAdm extends JFrame {
 	 * Create the frame.
 	 */
 	public RegisterAdm() {
+		connect = DBConnection.getConnection();
+		setType(Type.UTILITY);
+		setTitle("RegisterAdm");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 464);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		nameField = new JTextField();
+		nameField.setBounds(215, 81, 138, 20);
+		contentPane.add(nameField);
+		nameField.setColumns(10);
+		
+		lastnameField = new JTextField();
+		lastnameField.setColumns(10);
+		lastnameField.setBounds(215, 120, 138, 20);
+		contentPane.add(lastnameField);
+		
+		dniField = new JTextField();
+		dniField.setColumns(10);
+		dniField.setBounds(267, 188, 86, 20);
+		contentPane.add(dniField);
+		
+		phoneField = new JTextField();
+		phoneField.setColumns(10);
+		phoneField.setBounds(267, 219, 86, 20);
+		contentPane.add(phoneField);
+		
+		emailField = new JTextField();
+		emailField.setColumns(10);
+		emailField.setBounds(215, 250, 138, 20);
+		contentPane.add(emailField);
+		
+		JLabel lblNewLabel = new JLabel("Nombre");
+		lblNewLabel.setBounds(110, 87, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblLastname = new JLabel("Apellidos");
+		lblLastname.setBounds(110, 126, 46, 14);
+		contentPane.add(lblLastname);
+		
+		JLabel lblGender = new JLabel("Genero");
+		lblGender.setBounds(110, 160, 46, 14);
+		contentPane.add(lblGender);
+		
+		JLabel lblDni = new JLabel("DNI");
+		lblDni.setBounds(110, 191, 46, 14);
+		contentPane.add(lblDni);
+		
+		JLabel lblPhone = new JLabel("Celular");
+		lblPhone.setBounds(110, 225, 46, 14);
+		contentPane.add(lblPhone);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setBounds(110, 253, 46, 14);
+		contentPane.add(lblEmail);
+		
+		JLabel lblPassword = new JLabel("Contraseña");
+		lblPassword.setBounds(110, 287, 66, 14);
+		contentPane.add(lblPassword);
+		
+		JLabel lblNewLabel_1 = new JLabel("REGISTRO");
+		lblNewLabel_1.setBounds(190, 37, 56, 22);
+		contentPane.add(lblNewLabel_1);
+		
+		JComboBox<String> genderSelect = new JComboBox<String>();
+		genderSelect.setModel(new DefaultComboBoxModel<String>(new String[] {"Masculino", "Femenino"}));
+		genderSelect.setToolTipText("");
+		genderSelect.setBounds(267, 155, 86, 22);
+		contentPane.add(genderSelect);
+		
+		JButton RegisterButton = new JButton("Registrar");
+		RegisterButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = nameField.getText();
+				String lastname = lastnameField.getText();
+				String dni = dniField.getText();
+				String phone = phoneField.getText();
+				String email = emailField.getText();
+				String password = passwordField.getText();
+				String selectedGender = (String) genderSelect.getSelectedItem();
+				String genderMapping = "Masculino".equals(selectedGender) ? "Male" : "Female";
+				System.out.println("Género seleccionado: " + genderMapping);
+				
+				try {
+					String query = "INSERT INTO administrative (name, lastname, dni, phone, email, password, gender) VALUES (?,?,?,?,?,?,?)";
+					PreparedStatement st = connect.prepareStatement(query);
+					st.setString(1, name);
+					st.setString(2, lastname);
+					st.setString(3, dni);
+					st.setString(4, phone);
+					st.setString(5, email);
+					st.setString(6, password);
+					st.setString(7, genderMapping);
+					st.executeUpdate();
+					JOptionPane.showMessageDialog(null, "Registrado");
 
+					
+					
+				} catch (Exception err) {
+					err.printStackTrace();
+					JOptionPane.showMessageDialog(null, "Error");
+				}
+			}
+		});
+		RegisterButton.setBounds(189, 330, 89, 23);
+		contentPane.add(RegisterButton);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(267, 284, 86, 20);
+		contentPane.add(passwordField);
+	}
 }
